2cdc1df0b0e4d7b2b72555f19b106ebf8535af05
cdevansh043 <cdevansh043@gmail.com>
2025-08-29 12:23:19 +0530

    Day 9 Q18 Done

--- Day9-Q18.c
+++ Day9-Q18.c
@@ -7,13 +7,13 @@
 int main() {
     int marks;
     scanf("%d", &marks);
-    if (marks > 90) {
+    if (marks >= 90) {
         printf("A\n");
-    } else if (marks > 80 ) {
+    } else if (marks >= 80 ) {
         printf("B\n");
-    } else if (marks > 70 ) {
+    } else if (marks >= 70 ) {
         printf("C\n");
-    } else if (marks > 60 ) {
+    } else if (marks >= 60 ) {
         printf("D\n");
     } else if (marks > 50 ) {
         printf("E\n");
