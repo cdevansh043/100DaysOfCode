ff2152c96fbed4d48d44e043bd2b027df6ddfd1c
cdevansh043 <cdevansh043@gmail.com>
2025-09-05 12:47:20 +0530

    Day 16 Q31 Done

--- Day16-Q31.c
+++ Day16-Q31.c
@@ -20,12 +20,16 @@ int main() {
     int n;
     scanf("%d",&n);
     int binary=0, place=1;
+    //check if number is zero --> if it is print 0
     if (n==0) {
         printf("0");
         return 0;
-    } if (n < 0) {
+    }
+    //if negative changes to positive
+    if (n < 0) {
         n = -n;
     }
+    //binary conversion takes place here
     while (n > 0) {
         int remainder = n % 2;
         binary = binary + (remainder * place);
