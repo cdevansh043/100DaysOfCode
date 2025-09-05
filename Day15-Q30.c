f58e9b0e3e1cd54dcd64876b0cf387b328a3021e
cdevansh043 <cdevansh043@gmail.com>
2025-09-05 11:56:18 +0530

    Day 15 Q30 Done

--- Day15-Q30.c
+++ Day15-Q30.c
@@ -6,12 +6,12 @@
 int main() {
     int num;
     scanf("%d",&num);
-    int revered_num=0,remainder=0;
+    int reversed_num=0,remainder=0;
     while(num!=0) {
         remainder = num %  10; // 4 3 2 1
-        revered_num = (revered_num * 10) + remainder; //4 43 432 4321
+        reversed_num = (reversed_num * 10) + remainder; //4 43 432 4321
         num = num / 10;//123 12 1 0
     }
-    printf("%d",revered_num);
+    printf("%d",reversed_num);
     return 0;
 }
