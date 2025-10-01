50f2c76385038b2770b033357d27e4f084a90416
cdevansh043 <cdevansh043@gmail.com>
2025-10-01 16:15:02 +0530

    Day 42 Q83 Done

--- Day42-Q83.c
+++ Day42-Q83.c
@@ -9,16 +9,22 @@ Vowels=2, Consonants=3
 
 */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
     char str[100];
     scanf("%s",str);
+
+    for (int i = 0; i < strlen(str); i++) {
+        str[i] = tolower(str[i]);
+    }
+
     int vowel = 0;
     int consonant = 0;
     for (int k = 0; k < strlen(str); k++) {
-        if (str[k] == 'a' || str[k] == 'e' || str[k] == 'i' || str[k] == 'o' || str[k] == 'u' || str[k] = 'A'|| str[k] = 'E' || str[k] = 'I' || str[k] = 'O' || str[k] = 'U') {
+        if (str[k] == 'a' || str[k] == 'e' || str[k] == 'i' || str[k] == 'o' || str[k] == 'u') {
             vowel++;
         } else {
             consonant++;
