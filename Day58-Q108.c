10c26eb47a2eb28204308d88986f79aefe95a58b
cdevansh043 <cdevansh043@gmail.com>
2025-10-17 12:38:40 +0530

    Day 58 Q108 Done

--- Day58-Q108.c
+++ Day58-Q108.c
@@ -20,13 +20,15 @@ Output 2:
 int *productOthers(int nums[], int numsSize, int *returnSize) {
     *returnSize = numsSize;
     int *ans = (int *) malloc(sizeof(int) * (*returnSize));
-    for (int i = 0; i < numsSize; i++) {
-        ans[i] = 1;
-        for (int j = 0; j < numsSize; j++) {
-            if (j != i) {
-                ans[i] *= nums[j];
-            }
-        }
+
+    ans[0] = 1;
+    for (int i = 1; i < numsSize; i++) {
+        ans[i] = ans[i-1] * nums[i-1];
+    }
+    int suffix = 1;
+    for (int i = numsSize - 1; i >= 0 ; i--) {
+        ans[i] *= suffix;
+        suffix *= nums[i];
     }
     return ans;
 }
