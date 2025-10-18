b5698ab616c44d027a67873862f516de7fb2674a
cdevansh043 <cdevansh043@gmail.com>
2025-10-18 11:59:23 +0530

    Updated Logic

--- Day31-Q62.c
+++ Day31-Q62.c
@@ -19,8 +19,16 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int j = n-1; j >= 0; j--) {
-        printf("%d ", arr[j]);
+    int left = 0, right = n-1;
+    while (left <= right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
